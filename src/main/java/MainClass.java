7380a2efb10cd41961a2078f91ee15e2536e5d2b
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2017-02-07 10:36:08 -0400

    JavaCV set up

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,3 +1,7 @@
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+
 /**
  * Created by Menelaos Kotsollaris on 2017-02-06.
  *
@@ -5,4 +9,15 @@
  */
 public class MainClass
 {
+
+    public static void smooth(String filename)
+    {
+        IplImage image = cvLoadImage(filename);
+        if(image != null)
+        {
+            cvSmooth(image, image);
+            cvSaveImage(filename, image);
+            cvReleaseImage(image);
+        }
+    }
 }
